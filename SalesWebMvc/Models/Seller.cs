9b930af28058a0bd741b6252b8827377a92c32d1
LucassMota <lcsmota8@outlook.com>
2021-10-19 15:53:33 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,16 +8,22 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "{0} required")] 
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")] // "{0} é o nome do atributo e os números seguintes são os valores passados nos argumentos em ordem de declaração
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")] // "[Display]" é uma annotation que permite dar um apelido que pode ser exibido ao invés do nome do atributo
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
